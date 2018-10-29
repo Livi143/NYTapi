38a2e1ed35a2fafdbe9d0f633b289e00741c8fcc
Olivia Fontanese <olfo4481@colorado.edu>
2018-10-29 16:16:22 -0600

    nada

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*# New York Times Article Search - Phase 01
 
 ## Front-End Team
@@ -15,29 +14,36 @@
 * Do preliminary research on the [API](http://developer.nytimes.com/article_search_v2.json).
 
 * Register for an API Key.
+api key d2370443fa244a039a13b98495f3ed14
 
 * Understand what format the URL should look like to make an Article Call. (Hint: Use the API Console!!)
 
 * Experiment with console logging various fields.*/
-=======
-# New York Times Article Search - Phase 01
+
+
+
+
+/*# New York Times Article Search - Phase 02
 
 ## Front-End Team
-![nyt-search](Images/nyt.png)
 
-* Create the layout for the design above. Feel free to use Bootstrap or to do something more customized of your own.
+* Begin creating basic click events. Register the submit button
+// ajax call? 
+* Create working transfers of data between the text-boxes and the backend.
 
-* Make sure to organize your code so as to have the necessary IDs.
-
-* Incorporate text boxes for capturing User Input. Then research how to retrieve the input values in JavaScript
+* Experiment with creating content regions for where the article will go.
 
 ## Back-End Team
 
-* Do preliminary research on the [API](http://developer.nytimes.com/article_search_v2.json).
+* Create the AJAX call needed to retrieve data then console.log all of the relevant fields.*/
 
-* Register for an API Key.
+console.log(
 
-* Understand what format the URL should look like to make an Article Call. (Hint: Use the API Console!!)
+)
+//* Incorporate various “optional parameters” (hard code these in initially).
 
-* Experiment with console logging various fields.
->>>>>>> b00245bf2147138fcea354401538d1a7abbca8f1
+//* Take note of various “bugs” that appear with certain searches.
+
+//## All-Together
+
+//* Display the HTML content!!
